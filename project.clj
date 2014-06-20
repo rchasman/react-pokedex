a22ca5976fa19c8a5dbe06640e1bddb840364242
Roey Chasman <rchasman@email.arizona.edu>
2014-06-19 20:16:52 -0700

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject pokedex "0.1.0-SNAPSHOT"
+  :description "Reactive Pokedex"
+  :url "http://www.roeychasman.com/pokedex"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]
+                 [hiccup "1.0.5"]]
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler pokedex.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
---
+++ src/pokedex/handler.clj
@@ -0,0 +1,33 @@
+(ns pokedex.handler
+  (:use [compojure.core]
+        [hiccup.core]
+        [hiccup.page]
+        [hiccup.def])
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defhtml application [& body]
+  (html5
+    [:head
+        [:title "Reactive Pokedex"]
+        [:meta {:name "description" :content "View info about Pokemon reactively."}]
+        [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+        [:meta {:charset "UTF-8"}]]
+        [:link {:rel "stylesheet" :href "/css/styles.css"}]
+    [:body
+      [:script "var urlRoot = 'http://pokeapi.co/api/v1/'"]
+      [:script
+        {:data-main "js/main" :src "/js/scripts/require.js"}]
+      [:div#main]]))
+
+(defhtml not-found [& body]
+  (html5
+    [:h1 "Not Found"]))
+
+(defroutes app-routes
+  (GET "/" [] application)
+  (route/resources "/")
+  (route/not-found not-found))
+
+(def app
+  (handler/site app-routes))
