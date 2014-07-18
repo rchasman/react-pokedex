e2d85281af3e1b26ff1d3c0643914fbed50f1aad
Roey Chasman <rchasman@email.arizona.edu>
2014-07-18 10:47:29 -0700

    Added korma deps to project for clojure database functionality.

--- project.clj
+++ project.clj
@@ -3,9 +3,13 @@
   :url "http://www.roeychasman.com/pokedex"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
+                 [korma "0.3.0"]
                  [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler pokedex.handler/app}
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  {:dev
+    {:dependencies [
+      [javax.servlet/servlet-api "2.5"]
+      [org.xerial/sqlite-jdbc "3.7.15-M1"]
+      [ring-mock "0.1.5"]]}})
